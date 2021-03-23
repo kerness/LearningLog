91768bf53c50bad5480857a8d4ea93eb26d76312
Maciej Bąk <maciej.bak260@gmail.com>
2021-03-23 18:18:57 +0100

    adding new entries by user

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,9 +1,16 @@
-form django import forms
+from django import forms
 
-form .models import Topic
+from .models import Topic, Entry
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': 'Entry:'}
+        widgets = {'text': forms.Textarea(attrs = {'cols': 80})}
